1d68a6298c83e59ae2ee9d0a0247edd329274952
Tyler Varzeas <tyler.varzeas@gmail.com>
2025-12-03 20:15:10 -0800

    Add prisma

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1>This is a header</h1>
+      <Image
+        src={'/wulv.png'}
+        width={1000}
+        height={1000}
+        alt="Wulv Logo"
+        className="max-w-[400px] rounded-full mb-8"
+      />
+      <h1 className="text-6xl text-center font-bold">Wulv.io</h1>
+      <p className="text-xl text-center mt-4">Coming soon...</p>
     </div>
   )
 }
